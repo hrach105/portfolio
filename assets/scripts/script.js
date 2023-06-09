362c43ddf8237a3eb0bc2bde0846c6232cde4cfb
hrach105 <hrachkhachatryan24@gmail.com>
2023-06-09 23:08:16 +0400

    initial commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobileMenu');
+const close = document.getElementById('close');
+
+hamburger.addEventListener('click',function () {
+    mobileMenu.classList.toggle('toggle')
+})
+close.addEventListener('click',function () {
+    mobileMenu.classList.remove('toggle')
+})
+const swup = new Swup();
+AOS.init();
