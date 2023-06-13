dd35c6502f591b0ccfa4971f4bcf4862c897ef06
hrach105 <hrachkhachatryan24@gmail.com>
2023-06-13 19:43:52 +0400

    change cursor

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,48 @@
-const hamburger = document.getElementById('hamburger');
-const mobileMenu = document.getElementById('mobileMenu');
-const close = document.getElementById('close');
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+const close = document.getElementById("close");
 
-hamburger.addEventListener('click',function () {
-    mobileMenu.classList.toggle('toggle')
-})
-close.addEventListener('click',function () {
-    mobileMenu.classList.remove('toggle')
-})
+hamburger.addEventListener("click", function () {
+  mobileMenu.classList.toggle("toggle");
+});
+close.addEventListener("click", function () {
+  mobileMenu.classList.remove("toggle");
+});
 const swup = new Swup();
 AOS.init();
 
+var cursor = document.querySelector(".cursor");
+var cursorinner = document.querySelector(".cursor2");
+var a = document.querySelectorAll("a");
+
+document.addEventListener("mousemove", function (e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+});
+
+document.addEventListener("mousemove", function (e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorinner.style.left = x + "px";
+  cursorinner.style.top = y + "px";
+});
+
+document.addEventListener("mousedown", function () {
+  cursor.classList.add("click");
+  cursorinner.classList.add("cursorinnerhover");
+});
+
+document.addEventListener("mouseup", function () {
+  cursor.classList.remove("click");
+  cursorinner.classList.remove("cursorinnerhover");
+});
+
+a.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    cursor.classList.add("hover");
+  });
+  item.addEventListener("mouseleave", () => {
+    cursor.classList.remove("hover");
+  });
+});
